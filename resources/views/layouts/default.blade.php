ecb93c8da18e8f4fb7231c83d34db20e540be734
changhu <2946259283@qq.com>
2018-05-18 15:16:19 +0800

    finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','Sample App') - Laravel 学习教程</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
